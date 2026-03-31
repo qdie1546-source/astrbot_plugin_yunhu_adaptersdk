ce63be07a89a6177e320658f00e974f6e102e824
qdie1546-source <qdie1546@gmail.com>
2026-03-31 21:28:16 +0800

    更新 __init__.py

--- yunhu/__init__.py
+++ yunhu/__init__.py
@@ -1,1 +1,17 @@
+from .client import YunHuClient
+from .models import (
+    Message, TextMessage, ImageMessage, 
+    UserInfo, Event, MessageEvent
+)
+from .exceptions import (
+    YunHuError, YunHuAuthError, YunHuAPIError,
+    YunHuConnectionError, YunHuWebSocketError
+)
 
+__all__ = [
+    "YunHuClient",
+    "Message", "TextMessage", "ImageMessage",
+    "UserInfo", "Event", "MessageEvent",
+    "YunHuError", "YunHuAuthError", "YunHuAPIError",
+    "YunHuConnectionError", "YunHuWebSocketError",
+]
